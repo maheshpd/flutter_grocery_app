e5beae2c49ca93c42b16ab578ac360d432dba084
maheshprasad <mahesh223prasad@gmail.com>
2022-07-09 08:28:51 +0530

    Finish building the category screen

--- lib/widget/categories_widget.dart
+++ lib/widget/categories_widget.dart
@@ -4,7 +4,15 @@ import 'package:flutter_grocery_app/widget/text_widget.dart';
 import 'package:provider/provider.dart';
 
 class CategoriesWidget extends StatelessWidget {
-  const CategoriesWidget({Key? key}) : super(key: key);
+  final String catText, imgPath;
+  final Color passedcolor;
+
+  const CategoriesWidget(
+      {Key? key,
+      required this.catText,
+      required this.imgPath,
+      required this.passedcolor})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -13,26 +21,28 @@ class CategoriesWidget extends StatelessWidget {
     final Color color = themeState.getDarkTheme ? Colors.white : Colors.black;
 
     return InkWell(
-      onTap: (){
+      onTap: () {
         print('Category pressed');
       },
       child: Container(
         decoration: BoxDecoration(
-            color: Colors.red.withOpacity(0.1),
+            color: passedcolor.withOpacity(0.1),
             borderRadius: BorderRadius.circular(16),
-            border: Border.all(color: Colors.red.withOpacity(0.7), width: 2)),
+            border: Border.all(color: passedcolor.withOpacity(0.7), width: 2)),
         child: Column(
           children: [
             Container(
               height: _screenWidth * 0.3,
               width: _screenWidth * 0.3,
-              decoration: const BoxDecoration(
+              decoration: BoxDecoration(
                   image: DecorationImage(
-                      image: AssetImage('assets/images/cat/veg.png'),
+                      image: AssetImage(
+                        imgPath,
+                      ),
                       fit: BoxFit.fill)),
             ),
             TextWidget(
-              text: 'Category name',
+              text: catText,
               color: color,
               textSize: 20,
               isTitle: true,
